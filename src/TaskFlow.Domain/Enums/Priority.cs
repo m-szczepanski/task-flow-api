7b5d64144007ffcfd0e51885df6677a622f43a94
Mateusz <30538559+m-szczepanski@users.noreply.github.com>
2026-03-26 18:28:40 +0100

    feat: enums creation

---
+++ src/TaskFlow.Domain/Enums/Priority.cs
@@ -0,0 +1,9 @@
+namespace DefaultNamespace;
+
+public enum Priority
+{
+    Low = 0,
+    Medium = 1,
+    High = 2,
+    Critical = 3
+}
---
+++ src/TaskFlow.Domain/Enums/TaskStatus.cs
@@ -0,0 +1,10 @@
+namespace DefaultNamespace;
+
+public enum TaskStatus
+{
+    ToDo = 0,
+    InProgress = 1,
+    Done = 2,
+    Blocked = 3,
+    Cancelled = 4
+}
