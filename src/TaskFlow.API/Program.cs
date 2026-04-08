add09392df7714e049c6fb19f0f8916b5fe0c609
Mateusz <30538559+m-szczepanski@users.noreply.github.com>
2026-04-08 18:22:35 +0200

    feat: initial migration

--- src/TaskFlow.API/Program.cs
+++ src/TaskFlow.API/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
